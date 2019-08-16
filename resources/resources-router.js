11ce3e71ec79d003643589065e955da6ef3154f7
Wes Jonke <wesjonke@gmail.com>
2019-08-16 12:30:27 -0400

    filled out logic for resources router

---
+++ resources/resources-router.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const Resources = require('./resources-model');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  Resources.getResources()
+  .then(resources => {res.status(200).json(resources)})
+  .catch(err => {res.status(500).json(err)})
+});
+
+
+router.post('/', (req, res) => {
+  const resource = req.body
+  Resources.addResource(resource)
+  .then(resource => {res.status(200).json(resource)})
+  .catch(err => {res.status(500).json(err)})
+})
+  
+
+router.put('/:id', (req, res) => {
+  const changes = req.body
+  const {id} = req.params
+  Resources.updateResource(changes, id)
+  .then(resource => {res.status(200).json(resource)})
+  .catch(err => {res.status(500).json(err)})
+})
+
+
+router.delete('/:id', (req, res) => {
+const { id } = req.params;
+Resources.deleteResource(id)
+  .then(resource => {res.status(200).json(resource)})
+  .catch(err => {res.status(500).json(err)})
+})
+
+
+
+module.exports = router
